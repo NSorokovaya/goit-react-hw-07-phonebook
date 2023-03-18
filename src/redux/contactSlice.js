457072e112bee09d8fe81008c2a0145aea9e8050
NSorokovaya <nadiaasorokovaya@gmail.com>
2023-03-18 11:23:34 -0400

    Update contactSlice.js

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -10,7 +10,7 @@ const INITIAL_CONTACT_STATE = {
   filter: '',
 };
 
-const contactSlice = createSlice({
+const phonebookSlice = createSlice({
   name: 'contacts',
   initialState: INITIAL_CONTACT_STATE,
 
@@ -18,56 +18,54 @@ const contactSlice = createSlice({
     filterContact(state, { payload: filterValue }) {
       state.filter = filterValue;
     },
-    extraReducers: builder => {
-      builder
-        .addCase(fetchContactsList.pending, state => {
-          state.contacts.isLoading = true;
-          state.contacts.error = null;
-        })
-        .addCase(
-          fetchContactsList.fulfilled,
-          (state, { payload: contacts }) => {
-            state.contacts.items = contacts;
-            state.contacts.isLoading = false;
-          }
-        )
-        .addCase(fetchContactsList.rejected, (state, { payload: error }) => {
-          state.contacts.isLoading = false;
-          state.contacts.error = error;
-        })
+  },
 
-        .addCase(addContact.pending, state => {
-          state.contacts.isLoading = true;
-        })
-        .addCase(addContact.fulfilled, (state, { payload: newContact }) => {
-          state.contacts.items.push(newContact);
-          state.contacts.isLoading = false;
-        })
-        .addCase(addContact.rejected, (state, { payload: error }) => {
-          state.contacts.error = error;
-          state.contacts.isLoading = false;
-        })
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContactsList.pending, state => {
+        state.contacts.isLoading = true;
+        state.contacts.error = null;
+      })
+      .addCase(fetchContactsList.fulfilled, (state, { payload: contacts }) => {
+        state.contacts.items = contacts;
+        state.contacts.isLoading = false;
+      })
+      .addCase(fetchContactsList.rejected, (state, { payload: error }) => {
+        state.contacts.isLoading = false;
+        state.contacts.error = error;
+      })
 
-        .addCase(deleteContact.pending, state => {
-          state.isLoadingDelete = true;
-        })
-        .addCase(
-          deleteContact.fulfilled,
-          (state, { payload: deletedContactId }) => {
-            state.contacts.items = state.contacts.items.filter(
-              contact => contact.id !== deletedContactId
-            );
-            state.contacts.isLoading = false;
-          }
-        )
-        .addCase(deleteContact.rejected, (state, { payload }) => {
-          state.contacts.error = payload;
+      .addCase(addContact.pending, state => {
+        state.contacts.isLoading = true;
+      })
+      .addCase(addContact.fulfilled, (state, { payload: newContact }) => {
+        state.contacts.items.push(newContact);
+        state.contacts.isLoading = false;
+      })
+      .addCase(addContact.rejected, (state, { payload: error }) => {
+        state.contacts.error = error;
+        state.contacts.isLoading = false;
+      })
+
+      .addCase(deleteContact.pending, state => {
+        state.isLoadingDelete = true;
+      })
+      .addCase(
+        deleteContact.fulfilled,
+        (state, { payload: deletedContactId }) => {
+          state.contacts.items = state.contacts.items.filter(
+            contact => contact.id !== deletedContactId
+          );
           state.contacts.isLoading = false;
-        });
-    },
+        }
+      )
+      .addCase(deleteContact.rejected, (state, { payload }) => {
+        state.contacts.error = payload;
+        state.contacts.isLoading = false;
+      });
   },
 });
 
-export const { filterContact } = contactSlice.actions;
+export const { filterContact } = phonebookSlice.actions;
 
-export default contactSlice.reducer;
+export default phonebookSlice.reducer;
